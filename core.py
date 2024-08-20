81556c3a4563eddc8880aba84155c7c4acf65213
boytsovau <alexbonch@gmail.com>
2024-08-20 15:46:05 +0300

    modified:   core.py 	modified:   main.py

--- core.py
+++ core.py
@@ -4,40 +4,44 @@ from datetime import datetime
 
 class ExpireCert:
 
-    def __init__(self, hostname, username, password, port) -> None:
+    def __init__(self, hostname: str, username: str, password: str, port: int) -> None:
         self.hostname = hostname
         self.username = username
         self.password = password
         self.port = port
-
+        self.data = ""
 
     def make_con(self):
         try:
             self.client = paramiko.SSHClient()
             self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             self.client.load_system_host_keys()
-            self.client.connect(hostname=self.hostname, username=self.user, password=self.password, port=self.port)
-            self.con = self.client
+            self.client.connect(hostname=self.hostname, username=self.username, password=self.password, port=self.port)
+            return self.client
+            print(f"Succesful connected to {self.hostname}\n\n")
         except Exception as e:
-            return e
+            print(f"No connection to {self.hostname}: {e}\n\n")
 
     def send_command(self, command: str) -> str:
+        if not self.client:
+            return "No active connection."
         try:
-            self.make_con()
             self.command = command
-            stdin, stdout, stderr = self.con.exec_command(self.command)
+            stdin, stdout, stderr = self.client.exec_command(self.command)
             byte_data = stdout.read() + stderr.read()
-            data = str(byte_data, encoding ='utf-8')
-            return data
+            self.data = str(byte_data, encoding ='utf-8')
+            return self.data
         except Exception as e:
-            return e
+            print(f"Error: {e}")
+            return ""
 
-    def get_expire(self, days):
-        self.days = days
+    def get_expire(self, days: int) -> str:
+        if not self.data:
+            return "No data to process."
         result = [i for i in self.data.split('\n') if 'local' in i]
-        self.cert_dict =  {}
+        self.cert_dict = {}
         for i in result:
-            stdin, stdout, stderr = self.con.exec_command(f'cert_mgr show -i {i[0]}')
+            stdin, stdout, stderr = self.client.exec_command(f'cert_mgr show -i {i[0]}')
             data_string = (str(stdout.read() + stderr.read(), encoding = 'utf-8'))
 
             lines = data_string.split('\n')[1:]
@@ -45,9 +49,7 @@ class ExpireCert:
             for line in lines:
                 if ': ' in line:
                     key, value = line.split(': ', 1)
-                    key = key.strip()
-                    value = value.strip()
-                    self.result_dict[key] = value
+                    self.result_dict[key.strip()] = value.strip()
             self.cert_dict[i[0]] = self.result_dict
 
         for key, value in self.cert_dict.items():
@@ -56,21 +58,18 @@ class ExpireCert:
             if valid_to_str:
                 expire_date = datetime.strptime(valid_to_str, "%a %b %d %H:%M:%S %Y")
                 now = datetime.now().date()
-                day_exp = expire_date - now
-
-            if day_exp < self.days:
+                day_exp = (expire_date - now).days
+            if day_exp < days:
                 cert_subject = value.get('Subject')
                 cert_valid_date = value.get('Valid to')
                 cert_issuer = value.get('Issuer')
                 cert_serial = value.get('Serial number')
-                result = (f"Сертификат {cert_subject} просрочится через {self.days} дней.\n"
+                self.client.close()
+                return (f"Сертификат {cert_subject} просрочится через {day_exp} дней.\n\n"
                           f"Данные по сертификату:\n\n"
                           f"Subject: {cert_subject}\n"
                           f"Valid to: {cert_valid_date}\n"
                           f"Issuer: {cert_issuer}\n"
                           f"Serial: {cert_serial}")
-                self.con.close()
-                return result
-            else:
-                self.con.close()
-                return "Поле 'Valid to' не найдено в словаре."
+            self.con.close()
+            return f"Нет сертификатов с истекающим сроком действия более {days}"
--- main.py
+++ main.py
@@ -7,20 +7,22 @@ def main():
     parser.add_argument('--hostname', nargs='+', required=True, help="список ip")
     parser.add_argument('--username', required=True, help="Пользователь для подключения")
     parser.add_argument('--password', required=True, help="Пароль для подключения.")
-    parser.add_argument('--port', type=int, default=22, help="Порт для подключения.")
+    parser.add_argument('--port', type=int, default=22, help="Порт для подключения. По умолчанию 22")
     parser.add_argument('--command', type=str, required=True, help="команда для выполнения.")
-    parser.add_argument('--command', type=int, default=30, help="Дней до конца сертификата. По умолчанию 30")
+    parser.add_argument('--days', type=int, default=30, help="Дней до конца сертификата. По умолчанию 30")
 
     args = parser.parse_args()
 
     for host in args.hostname:
         try:
             con = ExpireCert(host, args.username, args.password, args.port)
+            con.make_con()
             con.send_command(args.command)
             exp = con.get_expire(args.days)
             print(f"{host}\n\n{exp}")
         except Exception as e:
             print(e)
+
 
 if __name__ == "__main__":
     main()
